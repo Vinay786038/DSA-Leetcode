552c2736e7590e4cc64ea769cd803838616a9db1
Vinay Yadav <vinayyadav7860385370@gmail.com>
2026-04-04 22:12:59 +0530

    Time: 0 ms (100.00%) | Memory: 35.4 MB (72.61%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.cpp
+++ 1428-jump-game-iii/jump-game-iii.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
-    bool DFS(vector<int>&arr,int i,vector<bool>&visited)
+    /*bool DFS(vector<int>&arr,int i,vector<bool>&visited)
     {
+        //DFS traversal
         int n=arr.size();
         if(i<0||i>=n||visited[i]==true)
         return false;
@@ -14,5 +15,37 @@ public:
     {
         vector<bool>visited(arr.size(),false);
         return DFS(arr,start,visited);
+    }*/
+    bool canReach(vector<int>& arr,int start)
+    {
+        //BFS traversal
+        int n=arr.size();
+        queue<int>q;
+        vector<bool>visited(n,false);
+        q.push(start);
+        visited[start]=true;
+        while(q.empty()!=true)
+        {
+            int i=q.front();
+            q.pop();
+            if(arr[i]==0)
+            return true;
+            int forw=i+arr[i];
+            int back=i-arr[i];
+            if(forw<n&&visited[forw]==false)
+            {
+                q.push(forw);
+                visited[forw]=true;
+            }
+            if(back>-1&&visited[back]==false)
+            {
+                q.push(back);
+                visited[back]=true;
+            }
+        }
+        return false;
+
+
+
     }
 };
