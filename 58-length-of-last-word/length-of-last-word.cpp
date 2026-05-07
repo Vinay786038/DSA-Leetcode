bf2ea630020f23efff097cc662d376a77488fea8
Vinay Yadav <vinayyadav7860385370@gmail.com>
2026-05-07 08:51:02 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (91.80%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s)
+    {
+        int n= s.size();
+        int length=0;
+        for(int i=n-1;i>=0;i--)
+        {
+            if(s[i]==' '&&length!=0)
+            break;
+            if(s[i]!=' ')
+            length++;
+            else
+            length=0;
+        }
+        return length;
+        
+    }
+};
