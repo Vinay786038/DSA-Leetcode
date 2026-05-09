6d6ac726256b56537f059d0097ef6e56884b11cf
Vinay Yadav <vinayyadav7860385370@gmail.com>
2026-05-09 23:06:33 +0530

    Time: 41 ms (90.84%) | Memory: 85.6 MB (92.27%) - LeetSync

---
+++ 575-distribute-candies/distribute-candies.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType)
+    {
+        int n=candyType.size();
+        sort(candyType.begin(),candyType.end());
+        int a=1;
+        for(int i=1;i<n;i++)
+        {
+            if(candyType[i-1]!=candyType[i])
+            {
+                a++;
+            }
+        }
+        if(a<n/2)
+        return a;
+        return n/2;
+
+    }
+};
