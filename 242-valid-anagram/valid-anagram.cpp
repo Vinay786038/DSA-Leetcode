65e837959beb0de0ac41b7e0ccc2def395efe0f5
Vinay Yadav <vinayyadav7860385370@gmail.com>
2026-06-02 15:59:05 +0530

    Time: 2 ms (49.95%) | Memory: 9.9 MB (24.96%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -4,10 +4,17 @@ public:
         if(s.size()!=t.size())
         return false;
         
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s!=t)
-        return false;
+        vector<int>freq(26,0);
+        for(int i=0;i<s.size();i++)
+        {
+            freq[s[i]-'a']++;
+            freq[t[i]-'a']--;
+        }
+        for(int i=0;i<26;i++)
+        {
+            if(freq[i]>0)
+            return false;
+        }
         return true;
         
     }
