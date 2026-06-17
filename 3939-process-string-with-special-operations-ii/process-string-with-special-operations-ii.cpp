cda8e938b40ace821d89352487e55eeeb8f285ad
Vinay Yadav <vinayyadav7860385370@gmail.com>
2026-06-17 10:59:10 +0530

    Time: 26 ms (63.22%) | Memory: 24.1 MB (51.72%) - LeetSync

---
+++ 3939-process-string-with-special-operations-ii/process-string-with-special-operations-ii.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    char processStr(string s, long long k) {
+
+        long long len = 0;
+
+        // Final length calculate karo
+        for(char c : s)
+        {
+            if(islower(c))
+                len++;
+            else if(c == '*')
+            {
+                if(len > 0) len--;
+            }
+            else if(c == '#')
+            {
+                len *= 2;
+            }
+            else if(c == '%')
+            {
+                // length unchanged
+            }
+        }
+
+        if(k >= len) return '.';
+
+        // Reverse process
+        for(int i = (int)s.size()-1; i >= 0; i--)
+        {
+            char c = s[i];
+
+            if(c == '#')
+            {
+                len /= 2;
+                if(k >= len)
+                    k -= len;
+            }
+            else if(c == '%')
+            {
+                k = len - 1 - k;
+            }
+            else if(c == '*')
+            {
+                len++;
+            }
+            else
+            {
+                len--;
+
+                if(k == len)
+                    return c;
+            }
+        }
+
+        return '.';
+    }
+};
