a493411e4bc570287465ae29075d3a544cf56588
Vinay Yadav <vinayyadav7860385370@gmail.com>
2026-07-03 23:35:25 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (15.80%) - LeetSync

--- 520-detect-capital/detect-capital.cpp
+++ 520-detect-capital/detect-capital.cpp
@@ -5,43 +5,13 @@ public:
         int n=word.size();
         if(n==1)
         return true;
-        int case1=0;
-        if(64<(int)word[0]&&(int)word[0]<91)
+        int count=0;
+        for(int i=0;i<n;i++)
         {
-            if(64<(int)word[1]&&(int)word[1]<91)
-            {
-                case1=1;
-            }
-            else
-            {
-                case1=3;
-            }
+            if(isupper(word[i]))
+            count++;
         }
-        if(case1==0)
-        {
-            for(int i=0;i<n;i++)
-            {
-                if(64<(int)word[i]&&(int)word[i]<91)
-                return false;
-            }
-        }
-        if(case1==1)
-        {
-            for(int i=2;i<n;i++)
-            {
-                if(96<(int)word[i]&&(int)word[i]<123)
-                return false;
-            }
-        }
-        if(case1==3)
-        {
-            for(int i=1;i<n;i++)
-            {
-                if(64<(int)word[i]&&(int)word[i]<91)
-                return false;
-            }
-        }
-        return true;
+        return (count==n||count==0||(count==1&&isupper(word[0])));
 
         
     }
