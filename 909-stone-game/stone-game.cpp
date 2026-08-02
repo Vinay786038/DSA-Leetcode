772a4b7f051d4ef8c3bec57773071610f6e67675
Vinay Yadav <vinayyadav7860385370@gmail.com>
2026-08-02 16:07:14 +0530

    Time: 20 ms (9.00%) | Memory: 19.6 MB (27.79%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
-    bool stoneGame(vector<int>& piles) {
-        return true;
+    int find(int i,int j,vector<int>&piles,vector<vector<int>>&dp)
+    {
+        if(i==j)
+        {
+            return piles[i];
+        }
+        if(dp[i][j]!=-1)
+        {
+            return dp[i][j];
+        }
+        int leftmax=piles[i]-find(i+1,j,piles,dp);
+        int rightmax=piles[j]-find(i,j-1,piles,dp);
+        return dp[i][j]=max(leftmax,rightmax);
+    }
+    bool stoneGame(vector<int>& piles)
+    {
+        int n=piles.size();
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        return find(0,n-1,piles,dp)>0;
     }
 };
