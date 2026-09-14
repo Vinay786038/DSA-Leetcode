e2ab30aa2e92be5fc0015fe3d4b718cc88d82f46
Vinay Yadav <vinayyadav7860385370@gmail.com>
2026-09-14 13:32:59 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (11.03%) - LeetSync

--- 866-rectangle-overlap/rectangle-overlap.cpp
+++ 866-rectangle-overlap/rectangle-overlap.cpp
@@ -2,22 +2,19 @@ class Solution {
 public:
     bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2)
     {
-        int ax1=rec1[0];
-        int ay1=rec1[1];
-        int ax2=rec1[2];
-        int ay2=rec1[3];
-        int bx1=rec2[0];
-        int by1=rec2[1];
-        int bx2=rec2[2];
-        int by2=rec2[3];
-        int left=max(ax1,bx1);
-        int right=min(ax2,bx2);
-        int bottom=max(ay1,by1);
-        int top=min(ay2,by2);
-        if(left<right&&bottom<top)
-        {
-            return true;
-        }
-        return false;
+        int left1=rec1[0];
+        int bottom1=rec1[1];
+        int right1=rec1[2];
+        int top1=rec1[3];
+        int left2=rec2[0];
+        int bottom2=rec2[1];
+        int right2=rec2[2];
+        int top2=rec2[3];
+        int left=max(left1,left2);
+        int right=min(right1,right2);
+        int bottom=max(bottom1,bottom2);
+        int top=min(top1,top2);
+        return (left<right)&&(bottom<top);
+        
     }
 };
