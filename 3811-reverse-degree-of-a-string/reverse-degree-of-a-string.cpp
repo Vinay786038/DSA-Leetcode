1c4252e09d505b17c03daba2463121f9657787b1
Vinay Yadav <vinayyadav7860385370@gmail.com>
2026-09-20 12:23:43 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (47.88%) - LeetSync

---
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverseDegree(string s)
+    {
+        int n=s.size();
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            int x=s[i]-'a';
+            int y=26-x;
+            sum+=y*(i+1);
+        }
+        return sum;
+        
+    }
+};
